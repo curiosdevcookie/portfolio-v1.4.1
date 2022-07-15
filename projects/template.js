a7796e19c5381f098a06a94b6cb5985469aea014
Ariadne Engelbrecht <ariadne.engelbrecht@thenativeweb.io>
2022-07-15 11:49:14 +0200

    Restructure file system

--- projects/template.js
+++
@@ -1,7 +0,0 @@
-const template = document.querySelector('template');
-const node = document.importNode(template.content, true);
-document.body.appendChild(node);
-
-
-
-//https://css-tricks.com/crafting-reusable-html-templates/
